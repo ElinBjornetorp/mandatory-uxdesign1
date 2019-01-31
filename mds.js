4c0099aa1b5a49971a4383933f42ae51a6f66aaf
unknown <bjornetorp@hotmail.com>
2019-01-31 12:38:37 +0100

    final commit

--- mds.js
+++ mds.js
@@ -1,6 +1,22 @@
 
 let mds = {
-  textField: function(element){},
+  textField: function(element){
+    //Finding elements
+    let input = element.querySelector('input');
+    let label = element.querySelector('p');
+
+    function onBlur(e) {
+      if(input.value) {
+        //Making the label stay in place
+        label.style.height = '20px';
+        label.style.fontSize = '12px';
+        label.style.color = 'black';
+      }
+    }
+
+    //Listening to blur
+    input.addEventListener('blur', onBlur);
+  },
   switch: function(element){},
   checkbox: function(element){},
   radioButton: function(element){},
